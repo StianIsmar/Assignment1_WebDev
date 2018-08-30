45a0db5e4e70ed3d35ad9c2b059c7e60c1d4acfe
Stian Ismar <stianismar@dhcp-10-22-16-122.wlan.ntnu.no>
2018-08-30 17:14:18 +0200

    Updated

--- canvas.js
+++ canvas.js
@@ -1,3 +1,33 @@
+
+
+var canvas=document.getElementById("canvas");
+   color=document.getElementById("color");
+    c=canvas.getContext("2d");
+    canvas.setAttribute('width','1000');
+    canvas.setAttribute('height','470');
+
+
+    
+    
+    c.beginPath();
+    c.arc(100,150,10,0,Math.PI*2,true);
+    c.closePath();
+    c.fill();
+    
+    
+    /*sirkel=new Sirkel();
+
+update()
+
+function update(){
+    c.clearRect(0,0,1000,1000)
+    sirkel.update()
+
+
+    requestAnimationFrame(update)
+}
+
+
 class Sirkel{
     constructor(){
         this.x=0
@@ -9,26 +39,12 @@ class Sirkel{
         c.fillStyle="lightgreen"
     }
     update(){
-      /*  c.fillStyle=color.value*/
+        c.fillStyle=color.value
         this.phase+=.1
         this.x=500+this.amp*Math.cos(this.phase)
         this.y=300+this.amp*Math.sin(this.phase)
-        
         c.beginPath()
         c.arc(this.x,this.y,this.r,0,Math.PI*2)
         c.fill()
     }
-}
-
-let canvas=document.getElementById("canvas"),
-    color=document.getElementById("color"),
-    c=canvas.getContext("2d"),
-    sirkel=new Sirkel();
-update()
-function update(){
-    c.clearRect(0,0,1000,1000)
-    sirkel.update()
-
-
-    requestAnimationFrame(update)
-}
+}*/
